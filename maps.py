34d2f64e2f4ecfb01678acd185460de99090b658
Elil Thirumugam <elilgrade8@gmail.com>
2026-03-15 18:54:49 -0400

    updated passwords to work on streamlit

--- maps.py
+++ maps.py
@@ -4,6 +4,10 @@ from pprint import pprint
 from dotenv import load_dotenv
 load_dotenv()
 MAPS_KEY = os.getenv('MAPS_KEY')
+if not MAPS_KEY:
+    # On Streamlit Cloud, load from environment variables set via Secrets
+    import streamlit as st
+    MAPS_KEY = st.secrets.get("MAPS_KEY")
 maps_access = googlemaps.Client(MAPS_KEY)
 print("HI")
 results = maps_access.find_place(input = "AIRINC APPLIED INFORMATICS & RESEARCH INC.",input_type="textquery")
